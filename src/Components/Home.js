569cb67ba4fba397f33540d38ebbe51c2c730f43
Griffin Ogura <griffin@ogura-family.com>
2020-11-10 16:57:52 -0500

    material-ui added, navbar added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 // you must always import React
+//import Grid from '@material-ui/core/Grid'
 
 export default function Home() {
     // you must export the component
     return (
       <>
-        <h1>Test 1</h1>
-        <h2>Welcome</h2>
+        <h1>Test</h1>
       </>
     );
   }
